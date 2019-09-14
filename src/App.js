9eaa3d941349d83dc71ad0cd78a1dbf520f1710d
gabriel.almeida <gabriel.rabelo@edu.pucrs.br>
2019-09-14 15:57:13 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Grid} from "@material-ui/core";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+      <Grid justify="center" container>
+          <Grid item xs={12}>
+              <Grid container spacing={5}>
+                  <Grid>
+
+                  </Grid>
+                  <Grid>
+
+                  </Grid>
+              </Grid>
+          </Grid>
+      </Grid>
   );
 }
 
